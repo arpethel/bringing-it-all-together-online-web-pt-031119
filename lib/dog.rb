c4314645d1f95e84772c65ebf8b4c8921d5bb690
Ryan Pethel <arpethel@gmail.com>
2019-05-25 18:44:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,5 +30,15 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grage)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    
+  end
 
 end
