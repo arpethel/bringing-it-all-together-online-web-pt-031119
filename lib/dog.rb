3befdca53a57423d27cafa6e478ea930c38fc010
Ryan Pethel <arpethel@gmail.com>
2019-05-25 20:35:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -89,7 +89,14 @@ class Dog
   end
 
   def self.find_or_create_by(name:, breed:)
-    
-  end
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Song.new(dog_data[0], dog_data[1], dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end 
 
 end
