a606f0033504bbcc0d121d83537e5586a0a8a8d8
Ryan Pethel <arpethel@gmail.com>
2019-05-25 18:24:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 class Dog
   attr_accessor :id, :name, :breed
-# binding.pry
+
   def initialize(id:, name:, breed:)
-    # binding.pry
     @id = id
     @name = name
     @breed = breed
