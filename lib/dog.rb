e22d64359592b75cdc990816cc6486a99a477081
Ryan Pethel <arpethel@gmail.com>
2019-05-25 18:31:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Dog
   attr_accessor :id, :name, :breed
 
-  def initialize(name:, breed:)
+  def initialize(name:, breed:, id:)
     @id = nil
     @name = name
     @breed = breed
