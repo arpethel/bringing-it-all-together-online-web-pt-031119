39ae50d4ebb0eabaf010da95e1da9ba29626d4e2
Ryan Pethel <arpethel@gmail.com>
2019-05-25 18:45:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,7 +37,7 @@ class Dog
     SQL
 
     DB[:conn].execute(sql, self.name, self.grage)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
 
   end
 
