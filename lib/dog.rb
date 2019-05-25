77d62d4223fbef27f26fc4fc45d77a2ef81b0372
Ryan Pethel <arpethel@gmail.com>
2019-05-25 18:36:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,13 @@ require 'pry'
 class Dog
   attr_accessor :id, :name, :breed
 
-  def initialize(name, breed)
-    binding.pry
+  def initialize(name:, breed:)
+    # binding.pry
     @id = id
     @name = name
     @breed = breed
   end
 
+binding.pry
 
 end
