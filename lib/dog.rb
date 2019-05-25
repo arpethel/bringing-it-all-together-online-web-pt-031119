e29af0f6af3ec3c0e3362bd7163f56c6720259c7
Ryan Pethel <arpethel@gmail.com>
2019-05-25 20:38:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -94,7 +94,7 @@ class Dog
     if !dog.empty?
       dog_data = dog[0]
       binding.pry
-      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+      dog = self.new(dog_data[0], dog_data[1], dog_data[2])
       # binding.pry
     else
       dog = self.create(name: name, breed: breed)
