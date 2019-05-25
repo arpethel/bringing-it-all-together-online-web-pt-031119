c68057d78c31394e813c686f5787030a357df088
Ryan Pethel <arpethel@gmail.com>
2019-05-25 20:21:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,8 +58,8 @@ class Dog
       # binding.pry
     dog_found = DB[:conn].execute(sql, id_num).flatten
 
-    binding.pry
-    # Dog.new(id: dog_found[0], name: dog_found[1], breed: dog_found[2])
+    # binding.pry
+    Dog.new(id: dog_found[0], name: dog_found[1], breed: dog_found[2])
   end
 
 end
