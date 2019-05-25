d610157c1be7c2bb7aa425acfb7c44084337854b
Ryan Pethel <arpethel@gmail.com>
2019-05-25 20:40:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -95,7 +95,7 @@ class Dog
       dog_data = dog[0]
       # binding.pry
       dog = self.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
-      binding.pry
+      # binding.pry
     else
       dog = self.create(name: name, breed: breed)
     end
